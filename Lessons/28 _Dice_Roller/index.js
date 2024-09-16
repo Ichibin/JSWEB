4812a57d4a3ff279aaa6b384b37921c239d355ef
Ernesto Saavedra <juanernestosaavedra@outlook.com>
2024-09-15 23:52:08 -0500

    uploading main page testing

---
+++ Lessons/28 _Dice_Roller/index.js
@@ -0,0 +1,23 @@
+
+
+function rollDice(){
+    const numDice = document.getElementById('numDice').value;
+    const diceResult =  document.getElementById('diceResult');
+    const diceImgs =  document.getElementById('diceImgs');
+
+    const values = [];
+    const images =[];
+
+    for(let i = 0; i < numDice;i++){
+        const value = Math.floor(Math.random() *6)+1;
+        // console.log(value);
+        values.push(value);
+        images.push(`<img src ="Dice_imgs/D${value}.png" alt="Dice Number ${value}">`)
+
+    }
+
+    diceResult.textContent = `Dice: ${values.join(', ')}`;
+
+    diceImgs.innerHTML = images.join('');
+
+} 
